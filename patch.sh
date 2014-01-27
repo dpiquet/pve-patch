f0457240be5899875160b06436ec3c2ed2879e10
PIQUET Damien <piqudam@gmail.com>
2014-01-27 16:54:37 +0100

    Update

--- patch.sh
+++ patch.sh
@@ -12,20 +12,20 @@
 #  Based on tolaris' patch:
 #  http://www.tolaris.com/2013/08/28/removing-the-proxmox-3-1-subscription-nag-dialog/
 
-echo "patching Proxmox VE"
+echo "Trying patching Proxmox VE"
 
-/usr/bin/patch /usr/share/pve-manager/ext4/pvemanagerlib.js -i - <<EOF
---- /usr/share/pve-manager/ext4/pvemanagerlib.js.orig   2013-08-28 10:33:15.000000000 +0100
-+++ /usr/share/pve-manager/ext4/pvemanagerlib.js        2013-08-28 10:37:52.000000000 +0100
-@@ -447,7 +447,7 @@
-            success: function(response, opts) {
-                var data = response.result.data;
-
--               if (data.status !== 'Active') {
-+               if (false && data.status !== 'Active') {
-                    Ext.Msg.show({
-                        title: gettext('No valid subscription'),
-                        icon: Ext.Msg.WARNING,
+/usr/bin/patch /usr/share/pve-manager/ext4/pvemanagerlib.js -p0 <<EOF
+--- /usr/share/pve-manager/ext4/pvemanagerlib.js	2013-10-23 07:02:55.000000000 +0200
++++ pvemanagerlib.js	2014-01-27 16:49:45.000000000 +0100
+@@ -451,7 +451,7 @@ Ext.define('PVE.Utils', { statics: {
+ 	    success: function(response, opts) {
+ 		var data = response.result.data;
+ 
+-		if (data.status !== 'Active') {
++		if (false && data.status !== 'Active') {
+ 		    Ext.Msg.show({
+ 			title: gettext('No valid subscription'),
+ 			icon: Ext.Msg.WARNING,
 EOF
 
 echo "Patch applied with status $?"
